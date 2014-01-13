a48f81805706f0eaf1d921fdcbb34739b7b59c87
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-13 15:42:29 +0900

    canFatorIntoPrimes -refactoring for clean code - change while to for

--- src/test/java/primefactors/PrimeFactorsTest.java
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -40,11 +40,9 @@ public class PrimeFactorsTest {
 	private List<Integer> of(int n) {
 		ArrayList<Integer> factors = new ArrayList<Integer>();
 
-		int divisor = 2;
-		while (n > 1) {
+		for (int divisor = 2; n > 1; divisor++) {
 			for (;n % divisor == 0; n /= divisor)
 				factors.add(divisor);
-			divisor++;
 		}
 
 		if (n > 1)
