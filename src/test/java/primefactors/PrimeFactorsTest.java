c4057b77d5dcdd1b17d182cfb502c85c120b19c3
{hongseongmin} <{hnsnmn@gmail.com}>
2014-01-13 09:24:13 +0900

    add PrimeFactorsTest test - add test method nothing() -  start with executable

---
+++ src/test/java/primefactors/PrimeFactorsTest.java
@@ -0,0 +1,17 @@
+package primefactors;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongseongmin
+ * Date: 2014. 1. 13.
+ * Time: 오전 9:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrimeFactorsTest {
+	@Test
+	public void nothing() {
+
+	}
+}
